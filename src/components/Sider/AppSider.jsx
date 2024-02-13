e61b70622ceb8cc79e04422eebaf3a49083db18d
Alabuzhev Daniil <90332154+Kuroky79@users.noreply.github.com>
2024-02-13 19:48:38 +0300

    ...

--- src/components/Sider/AppSider.jsx
+++ src/components/Sider/AppSider.jsx
@@ -1,12 +1,62 @@
-import {Layout} from "antd";
+import {Layout, Card, Statistic,List,Typography} from "antd";
+import { ArrowDownOutlined, ArrowUpOutlined } from '@ant-design/icons';
+import {useEffect, useState} from "react";
+import {cryptoAssets, cryptoData} from "../../data/data.js";
 const siderStyle = {
-    textAlign: 'center',
-    lineHeight: '120px',
-    color: '#fff',
-    backgroundColor: '#1677ff',
+    padding: '1rem',
+
 };
+const data = [
+    'Racing car sprays burning fuel into crowd.',
+    'Japanese princess to wed commoner.',
+    'Australian walks 100km after outback crash.',
+    'Man charged over missing wedding girl.',
+    'Los Angeles battles huge wildfires.',
+];
 export default function AppSider(){
+    const [loading,setLoading] = useState(false);
+    const [crypto,setCrypto] = useState([]);
+    const [assets, setAssets] = useState([]);
+
+    useEffect(() => {
+        async function preload(){
+            setLoading(true);
+            const {result} = await cryptoData();
+            const assets = await cryptoAssets();
+        }
+    }, []);
     return (<Layout.Sider width="25%" style={siderStyle}>
-        Sider
+        <Card bordered={false} style={{marginBottom: 10}}>
+            <Statistic
+                title="Active"
+                value={11.28}
+                precision={2}
+                valueStyle={{ color: '#3f8600' }}
+                prefix={<ArrowUpOutlined />}
+                suffix="%"
+            />
+            <List
+                size='small'
+                bordered
+                dataSource={data}
+                renderItem={(item) => (
+                    <List.Item>
+                        <Typography.Text mark>[ITEM]</Typography.Text> {item}
+                    </List.Item>
+                )}
+            />
+        </Card>
+        <Card bordered={false}>
+            <Statistic
+                title="Idle"
+                value={9.3}
+                precision={2}
+                valueStyle={{
+                    color: '#cf1322',
+                }}
+                prefix={<ArrowDownOutlined />}
+                suffix="%"
+            />
+        </Card>
     </Layout.Sider>)
 }
