8fbb407d523a80c795cbbfbcae6c006310f9bf7f
Alabuzhev Daniil <90332154+Kuroky79@users.noreply.github.com>
2024-02-13 20:49:51 +0300

    ...

--- src/components/Sider/AppSider.jsx
+++ src/components/Sider/AppSider.jsx
@@ -3,17 +3,13 @@ import { ArrowDownOutlined, ArrowUpOutlined } from '@ant-design/icons';
 import {useEffect, useState} from "react";
 import {cryptoAssets, cryptoData} from "../../data/data.js";
 import {fakeFetchAssets, fakeFetchCrypto} from "../../data/api.js";
+import {percentDifference} from "../utils/utils.js";
 const siderStyle = {
     padding: '1rem',
-
 };
-const data = [
-    'Racing car sprays burning fuel into crowd.',
-    'Japanese princess to wed commoner.',
-    'Australian walks 100km after outback crash.',
-    'Man charged over missing wedding girl.',
-    'Los Angeles battles huge wildfires.',
-];
+
+
+
 export default function AppSider(){
     const [loading,setLoading] = useState(false);
     const [crypto,setCrypto] = useState([]);
@@ -25,7 +21,16 @@ export default function AppSider(){
             const {result} = await fakeFetchCrypto();
             const assets = await fakeFetchAssets();
             setCrypto(result);
-            setAssets(assets);
+            setAssets(assets.map(asset => {
+                const coin = result.find((c) => c.id === asset.id);
+                return {
+                    grow: asset.price < coin.price,
+                    growPercent: percentDifference(asset.price, coin.price),
+                    totalAmount: asset.amount * coin.price,
+                    totalProfit: asset.amount * coin.price - asset.amount * asset.price,
+                    ...asset
+                }
+            }));
             setLoading(false);
         }
         preload();
@@ -34,38 +39,48 @@ export default function AppSider(){
     if (loading){
         return <Spin fullscreen/>
     }
-    return (<Layout.Sider width="25%" style={siderStyle}>
-        <Card bordered={false} style={{marginBottom: 10}}>
-            <Statistic
-                title="Active"
-                value={11.28}
-                precision={2}
-                valueStyle={{ color: '#3f8600' }}
-                prefix={<ArrowUpOutlined />}
-                suffix="%"
-            />
-            <List
-                size='small'
-                bordered
-                dataSource={data}
-                renderItem={(item) => (
-                    <List.Item>
-                        <Typography.Text mark>[ITEM]</Typography.Text> {item}
-                    </List.Item>
-                )}
-            />
-        </Card>
-        <Card bordered={false}>
-            <Statistic
-                title="Idle"
-                value={9.3}
-                precision={2}
-                valueStyle={{
-                    color: '#cf1322',
-                }}
-                prefix={<ArrowDownOutlined />}
-                suffix="%"
-            />
-        </Card>
+    return (
+        <Layout.Sider width="25%" style={siderStyle}>
+            {assets.map((asset) => (
+                <Card key={asset.id} bordered={false} style={{marginBottom: 10}}>
+                    <Statistic
+                        title={asset.id}
+                        value={asset.totalAmount}
+                        precision={2}
+                        valueStyle={{ color: asset.grow ? '#3f8600' : '#cf1322'}}
+                        prefix={asset.grow ? <ArrowUpOutlined /> : <ArrowDownOutlined/>}
+                        suffix="$"
+                    />
+                    <List
+                        size='small'
+                        bordered
+                        dataSource={[
+                            {title: 'Total Profit', value: asset.totalProfit},
+                            {title: 'Asset Amount', value: asset.amount, isPlain: true},
+                            {title: 'Difference', value: asset.growPercent}
+                        ]}
+                        renderItem={(item) => (
+                            <List.Item>
+                                <span>{item.title}</span>
+                                {item.isPlain && <span>{item.value}</span>}
+                                {!item.isPlain && <span>{item.value.toFixed(2)}$</span>}
+                            </List.Item>
+                        )}
+                    />
+                </Card>
+            ))}
+
+        {/*<Card bordered={false}>*/}
+        {/*    <Statistic*/}
+        {/*        title="Idle"*/}
+        {/*        value={9.3}*/}
+        {/*        precision={2}*/}
+        {/*        valueStyle={{*/}
+        {/*            color: '#cf1322',*/}
+        {/*        }}*/}
+        {/*        prefix={<ArrowDownOutlined />}*/}
+        {/*        suffix="%"*/}
+        {/*    />*/}
+        {/*</Card>*/}
     </Layout.Sider>)
 }
