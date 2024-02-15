5b8fcf763af5acf3dc148fe864e949752874aa96
Alabuzhev Daniil <90332154+Kuroky79@users.noreply.github.com>
2024-02-15 14:23:42 +0300

    upgradeModal

--- src/components/Modal/CryptoInfoModal.jsx
+++ src/components/Modal/CryptoInfoModal.jsx
@@ -1,5 +1,41 @@
-
+import {Flex, Tag, Typography,Divider} from 'antd';
 
 export default function CryptoInfoModal({coin}){
-    return (<h2>{coin.name}</h2>)
+    return (
+        <>
+            <Flex style={{alignItems: 'center'}}>
+                <img src={coin.icon} alt={coin.name} style={{width: 40}}/>
+                <Typography.Title level={2} style={{margin: 0, marginLeft: 15}}>({coin.symbol}) {coin.name}</Typography.Title>
+            </Flex>
+            <Divider/>
+            <Typography.Paragraph>
+                <Typography.Text strong>1 hour: </Typography.Text>
+                <Tag color={coin.priceChange1h > 0 ? 'green' : 'red'}>{coin.priceChange1h}%</Tag>
+            </Typography.Paragraph>
+            <Typography.Paragraph>
+                <Typography.Text strong>1 day: </Typography.Text>
+                <Tag color={coin.priceChange1d > 0 ? 'green' : 'red'}>{coin.priceChange1d}%</Tag>
+            </Typography.Paragraph>
+            <Typography.Paragraph>
+                <Typography.Text strong>1 week: </Typography.Text>
+                <Tag color={coin.priceChange1w > 0 ? 'green' : 'red'}>{coin.priceChange1w}%</Tag>
+            </Typography.Paragraph>
+            <Typography.Paragraph>
+                <Typography.Text strong>Price: </Typography.Text>
+                <Tag>{(coin.price).toFixed(2)}$</Tag>
+            </Typography.Paragraph>
+            <Typography.Paragraph>
+                <Typography.Text strong>Price BTC: </Typography.Text>
+                <Tag>{coin.priceBtc.toFixed(2)}$</Tag>
+            </Typography.Paragraph>
+            <Typography.Paragraph>
+                <Typography.Text strong>Market Cap: </Typography.Text>
+                <Tag>{coin.marketCap.toFixed(2)}$</Tag>
+            </Typography.Paragraph>
+            <Typography.Paragraph>
+                <Typography.Text strong>contractAddress: </Typography.Text>
+                <Tag>{coin.contractAddress}$</Tag>
+            </Typography.Paragraph>
+        </>
+    )
 }
